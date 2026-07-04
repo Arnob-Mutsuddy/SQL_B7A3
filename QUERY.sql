cca68a64d6109fb6bc1ae60390ea7d098d878dc4
Arnob-Mutsuddy <arnobmutsuddy11@gmail.com>
2026-07-04 20:37:29 +0600

    query 3

--- QUERY.sql
+++ QUERY.sql
@@ -81,3 +81,8 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 --Query 2
 SELECT user_id, full_name, email FROM Users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+--Query 3
+SELECT booking_id, user_id, match_id,
+COALESCE(payment_status,'Action Required') AS systematic_status FROM Bookings
+WHERE payment_status IS NULL;
