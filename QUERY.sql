90029762243072cd0e744602a834fc7f979111f6
Arnob-Mutsuddy <arnobmutsuddy11@gmail.com>
2026-07-04 20:26:00 +0600

    query 2

--- QUERY.sql
+++ QUERY.sql
@@ -77,3 +77,7 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 --Query 1
 SELECT match_id, fixture, base_ticket_price FROM Matches
 WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+--Query 2
+SELECT user_id, full_name, email FROM Users
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
